5350f9ee36f68ec947610c95e010c7e3ea8960e4
Lindsay Wang <tascape@gmail.com>
2015-07-30 00:52:01 -0700

    update sample test cases

--- src/main/java/com/tascape/qa/grws/test/GithubRepoTests.java
+++ src/main/java/com/tascape/qa/grws/test/GithubRepoTests.java
@@ -18,8 +18,10 @@ package com.tascape.qa.grws.test;
 import com.tascape.qa.th.driver.TestDriver;
 import com.tascape.qa.th.test.AbstractTest;
 import com.tascape.qa.grws.driver.GithubRepoWebService;
+import java.util.concurrent.TimeUnit;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,18 +37,23 @@ public class GithubRepoTests extends AbstractTest {
     private final GithubRepoWebService service;
 
     public GithubRepoTests() {
+        this.globalTimeout = new Timeout(10, TimeUnit.SECONDS);
         this.service = this.getEntityDriver(GITHUB_SERVICE, GithubRepoWebService.class);
     }
 
     @Test
     public void testStatus() throws Exception {
+        long start = System.currentTimeMillis();
         String status = this.service.getStatus();
+        this.putResultMetric("Github", "load-status-page", System.currentTimeMillis() - start);
         Assert.assertEquals("error status", "OK", status);
     }
 
     @Test
     public void testIssues() throws Exception {
+        long start = System.currentTimeMillis();
         String issues = this.service.getIssues();
+        this.putResultMetric("Github", "load-issues-page", System.currentTimeMillis() - start);
         LOG.debug("\n{}", issues);
         Assert.assertTrue("did not find expected info,", issues.contains("New issue"));
     }
